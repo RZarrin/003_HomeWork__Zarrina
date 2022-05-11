50130a3c6ac0175534f61febea52e56ea8f6bf94
RZarrin <103204204+RZarrin@users.noreply.github.com>
2022-05-11 15:42:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+var num = +prompt ('Введите время в цифрах');
+switch(num) {
+    case 1:
+        alert('1 (Час) ночи');
+        break;
+    case 2:
+        alert('2 (Два) часа ночи');
+        break;
+    case 3:
+        alert ('3 (Три) часа ночи');
+        break;
+    case 4:
+        alert('4 (Четыре) часа ночи');
+        break;
+    case 5:
+        alert('5 (Пять) часов ночи');
+        break;
+    case 6:
+        alert('6 (Шесть) часов утра');
+        break;
+    case 7:
+        alert('7 (Семь) часов утра');
+        break;
+    case 8:
+        alert('8 (Восемь) часов утра');
+        break;
+    case 9:
+        alert('9 (Девять) часов утра');
+        break;
+    case 10:
+        alert('10 (Десять) часов утра');
+        break;
+    case 11:
+        alert('11 (Одинадцать) часов утра');
+        break;
+    case 12:
+        alert('12 (Двенадцать) часов дня');
+        break;
+    case 13:
+        alert('1 (Час) дня');
+        break;
+    case 14:
+        alert('2 (Два) часа дня');
+        break;
+    case 15:
+        alert('3 (Три) часа дня');
+        break;
+    case 16:
+        alert('4 (Четыре) часа дня');
+        break;
+    case 17:
+        alert('5 (Пять) часов дня');
+        break;
+    case 18:
+        alert('6 (Шесть) часов вечера');
+        break;
+    case 19:
+        alert('7 (Семь) часов вечера');
+        break;
+    case 20:
+        alert('8 (Восемь) часов вечера');
+        break;
+    case 21:
+        alert('9 (Девять) часов вечера');
+        break;
+    case 22:
+        alert('10 (Десять) часов вечера');
+        break;
+    case 23:
+        alert('11 (Одинадцать) часов вечера');
+        break;
+    case 24:
+        alert('12 (Двенадцать) часов ночи');
+    default:
+        alert('Что то пошло не так');
+}
